818b7367bd0bb4fe1f02616a43ab94943bb70d6a
Gustavo Silveira Silva <silva.gustavo@hotmail.com.br>
2025-02-05 21:21:02 -0300

    atualizacao aula2

--- src/banco/Transacao.ts
+++ src/banco/Transacao.ts
@@ -2,40 +2,60 @@ import { Cliente } from "./Cliente";
 import { ContaCorrente } from "./ContaCorrente";
 import { ContaPoupanca } from "./ContaPoupanca";
 
-const cliente = new Cliente(
-  "Cezar",
-  "123455234320",
-  "cezar@email.com",
-  new Date("1990-07-12")
-);
+function main() {
+  const cliente = new Cliente(
+    "Cezar",
+    "cezar@email.com",
+    new Date("1990-07-12")
+  );
 
-const conta = new ContaCorrente(1233, 12311, cliente, new Date("2009-06-06"));
+  cliente.setDocumento("123455234320");
 
-const contaPoupanca = new ContaPoupanca(
-  1233,
-  12311,
-  cliente,
-  new Date("2009-06-06")
-);
+  const conta = new ContaCorrente(1233, 12311, cliente, new Date("2009-06-06"));
 
-contaPoupanca.updateSaldo(30, "C");
+  const contaPoupanca = new ContaPoupanca(
+    1233,
+    12311,
+    cliente,
+    new Date("2009-06-06")
+  );
 
-contaPoupanca.updateSaldo(20, "D");
+  contaPoupanca.updateSaldo(30, "C");
 
-const saldoPoupanca = contaPoupanca.getSaldo();
-console.log(`Saldo conta poupanca: ${saldoPoupanca}`);
+  contaPoupanca.updateSaldo(20, "D");
 
-const contaCorrente = new ContaCorrente(
-  1234,
-  123123,
-  cliente,
-  new Date("2010-01-01")
-);
+  const saldoPoupanca = contaPoupanca.getSaldo();
+  console.log(`Saldo conta poupanca: ${saldoPoupanca}`);
 
-contaCorrente.updateSaldo(20, "C");
-console.log(`Saldo da conta corrente: ${contaCorrente.getSaldo()}`);
+  const contaCorrente = new ContaCorrente(
+    1234,
+    123123,
+    cliente,
+    new Date("2010-01-01")
+  );
 
-contaCorrente.updateSaldo(30, "D");
-contaCorrente.updateSaldo(9.99, "C");
-contaCorrente.getExtrato();
-console.log(`Saldo da conta corrente: ${contaCorrente.getSaldo()}`);
+  contaCorrente.updateSaldo(20, "C");
+  console.log(`Saldo da conta corrente: ${contaCorrente.getSaldo()}`);
+
+  contaCorrente.updateSaldo(30, "D");
+  contaCorrente.updateSaldo(8, "C");
+
+  contaCorrente.updateSaldo(110000, "D");
+
+  try {
+    contaCorrente.updateSaldo(8, "F");
+  } catch (error) {
+    throw error;
+  }
+
+  contaCorrente.getExtrato();
+  console.log(`Saldo da conta corrente: ${contaCorrente.getSaldo()}`);
+}
+
+(() => {
+  try {
+    main();
+  } catch (error) {
+    console.log(error);
+  }
+})();
